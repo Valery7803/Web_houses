ca0f37dc2b660df172f5f76b2f8d5933d93686b0
Valery <125111692+Valery7803@users.noreply.github.com>
2024-10-22 16:12:13 +0300

    Добавил функцию исключения ошибка 404

--- web_houses/houses/views.py
+++ web_houses/houses/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
+menu = [
+    'Главная страница', 'Страница про нас', 'Наши контакты', 'Авторизация'
+]
+
+# Представление 'главной страницы'
 def index(request):
     return render(request, 'houses/index.html')
 
-
+# Представление 'страницы про нас'
 def about(request):
     return render(request, 'houses/about.html')
 
-
+# Представление страницы 'наши контакты'
 def contacts(request):
     return render(request, 'houses/contacts.html')
 
-
+# Представление страницы 'обзора дома'
 def show_house(request, show_id):
     return HttpResponse(f'Обзор дома с {show_id}')
 
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
+# Страница 'авторизации'
 def login(request):
     return HttpResponse('Авторизация')
