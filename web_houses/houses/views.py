636b341c3a23ce3c8a7068120408298bf2ddee24
Valery <125111692+Valery7803@users.noreply.github.com>
2024-10-23 09:36:27 +0300

    Создал шаблоны для HTML

--- web_houses/houses/views.py
+++ web_houses/houses/views.py
@@ -5,13 +5,34 @@ menu = [
     'Главная страница', 'Страница про нас', 'Наши контакты', 'Авторизация'
 ]
 
+data_db = [
+    {'id': 1, 'title': 'Коттедж «Ривьера 1»', 
+     'context':'Премиальные двухэтажные коттеджи «Ривьера»: панорамные окна, просторная терраса и два отдельных входа',
+     'prise': '7 474 050₽'},
+    {'id': 2, 'title': 'Коттеджи «Император 1»', 
+     'context': 'Коттеджи «Император» — строгая геометрия, просторные помещения и никаких лишних деталей.',
+     'prise': '9 149 400₽'},
+    {'id': 3, 'title': 'Коттедж «Традиция 1»', 
+     'context': 'Коттеджи «Традиция» — шале со вторым светом и большим выбором планировок.',
+     'prise': '9 530 200₽'},
+]
+
 # Представление 'главной страницы'
 def index(request):
-    return render(request, 'houses/index.html')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'home': data_db,
+        }
+    return render(request, 'houses/index.html', data)
 
 # Представление 'страницы про нас'
 def about(request):
-    return render(request, 'houses/about.html')
+    data = {
+        'title': 'Страница про нас',
+        'menu': menu,
+        }
+    return render(request, 'houses/about.html', context = data)
 
 # Представление страницы 'наши контакты'
 def contacts(request):
